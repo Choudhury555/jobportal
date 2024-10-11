0b5ab5101709eeda855cc831e66ce9123bb8e087
Choudhury555 <choudhurypanda143@gmail.com>
2024-10-12 02:53:28 +0530

    initial frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-500">Jay Shree Ram</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
