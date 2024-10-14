f7f33261a3224322eefb857ad365c3533de5c66c
Choudhury555 <choudhurypanda143@gmail.com>
2024-10-14 23:41:04 +0530

    dummy check-in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-red-500">Jay Shree Ram</h1>
-      <Button>Click me</Button>
-      {/* Navbar code will start */}
+      
     </>
   )
 }
