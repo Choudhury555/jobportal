5ec5aff244a9869ff140507eb43ee9c88fbe3f42
Choudhury555 <choudhurypanda143@gmail.com>
2024-10-18 03:53:24 +0530

    Login page check-in

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Navbar from '../shared/Navbar'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Navbar />
+      <div className='flex items-center justify-center max-w-7xl mx-auto'>
+        <form action="" className='w-1/2 border border-gray-200 rounded p-4 my-10' >
+          <h1 className='font-bold text-xl mb-5'>Login</h1>
+          <div className='my-2'>
+            <Label>Email</Label>
+            <Input type="Email" placeholder="Enter Your Email" />
+          </div>
+          <div className='my-2'>
+            <Label>Password</Label>
+            <Input type="password" placeholder="Enter Your Password" />
+          </div>
+          <div className='flex items-center justify-between'>
+            <RadioGroup className='flex items-center gap-4 my-5'>
+              <div className="flex items-center space-x-2">
+                <Input type="radio" name="role" value="student" className="cursor-pointer" />
+                <Label>Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input type="radio" name="role" value="recruiter" className="cursor-pointer" />
+                <Label>Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <Button type="submit" className="w-full my-4">Login</Button>
+          <span className='text-sm'>Don't have an account.<Link to={"/signup"} className="text-blue-600">Signup</Link></span>
+        </form>
+      </div>
+    </div>
   )
 }
 
