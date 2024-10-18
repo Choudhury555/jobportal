c0cecde297655975163c068fa9a8aa93abf744aa
Choudhury555 <choudhurypanda143@gmail.com>
2024-10-18 17:46:02 +0530

    Login/Signup page get data from the FORM through useState

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
@@ -7,28 +7,43 @@ import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+
+  const inputChangeHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value })
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  }
+
   return (
     <div>
       <Navbar />
       <div className='flex items-center justify-center max-w-7xl mx-auto'>
-        <form action="" className='w-1/2 border border-gray-200 rounded p-4 my-10' >
+        <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded p-4 my-10' >
           <h1 className='font-bold text-xl mb-5'>Login</h1>
           <div className='my-2'>
             <Label>Email</Label>
-            <Input type="Email" placeholder="Enter Your Email" />
+            <Input type="Email" value={input.email} name="email" onChange={inputChangeHandler} placeholder="Enter Your Email" />
           </div>
           <div className='my-2'>
             <Label>Password</Label>
-            <Input type="password" placeholder="Enter Your Password" />
+            <Input type="password" value={input.password} name="password" onChange={inputChangeHandler} placeholder="Enter Your Password" />
           </div>
           <div className='flex items-center justify-between'>
             <RadioGroup className='flex items-center gap-4 my-5'>
               <div className="flex items-center space-x-2">
-                <Input type="radio" name="role" value="student" className="cursor-pointer" />
+                <Input type="radio" name="role" value="student" checked={input.role === 'student'} onChange={inputChangeHandler} className="cursor-pointer" />
                 <Label>Student</Label>
               </div>
               <div className="flex items-center space-x-2">
-                <Input type="radio" name="role" value="recruiter" className="cursor-pointer" />
+              <Input type="radio" name="role" value="recruiter" checked={input.role==='recruiter'} onChange={inputChangeHandler} className="cursor-pointer" />
                 <Label>Recruiter</Label>
               </div>
             </RadioGroup>
