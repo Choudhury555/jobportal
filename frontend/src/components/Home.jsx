fc86e93ff10aa075941ff1b67ea4f5ecdb8f5907
Choudhury555 <choudhurypanda143@gmail.com>
2024-11-17 02:28:13 +0530

    ADMIN control started for UI and Companies , CompaniesTable components added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
 import Footer from './shared/Footer'
 import useGetAllJobs from '@/hooks/useGetAllJobs'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   useGetAllJobs();//calling custom hooks (like calling a function)
+  //below code till return we have to do it here(previously my thinking was to navigate in <Login /> page to '/admin/companies' if role is 'recruiter' but but but that's not the right way)
+  const {user} = useSelector(store => store.auth);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    //after login our code will come to this component <Home /> and here it will run this "useEffect" first(if role is 'recruiter' then it will navigate to '/admin/companies')
+    if(user.role === 'recruiter'){
+      navigate("/admin/companies");
+    }
+  },[])
+
   return (
     <div>
         <Navbar />
