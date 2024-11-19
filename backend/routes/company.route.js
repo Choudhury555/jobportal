2cbbeca183ed70c3752b0e3132d8c0e6f88e3684
Choudhury555 <choudhurypanda143@gmail.com>
2024-11-20 03:24:28 +0530

    CompanySetup/update API integration and useGetCompanyById custom hooks for fetch Company By Id  and multer for file(multipart/form-data) in company.route.js and cloudinary for image upload in company.controller.js

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,12 +1,13 @@
 import express from "express"
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { getCompany, getCompanyById, registerCompany, updateCompany } from "../contollers/company.controller.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router  = express.Router();
 
 router.route("/register").post(isAuthenticated,registerCompany);
 router.route("/getcompany").get(isAuthenticated,getCompany);
 router.route("/getcompany/:id").get(isAuthenticated,getCompanyById);
-router.route("/update/:id").put(isAuthenticated,updateCompany);
+router.route("/update/:id").put(isAuthenticated,singleUpload,updateCompany);
 
 export default router;
