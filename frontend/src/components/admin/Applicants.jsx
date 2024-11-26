6a6d171a7c5ada4dd4d32e9dcddf3cf6080a7621
Choudhury555 <choudhurypanda143@gmail.com>
2024-11-27 00:37:58 +0530

    Dynamic value in ApplicantsTable component and applicationSlice added in redux

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../shared/Navbar'
 import ApplicantsTable from './ApplicantsTable'
+import axios from 'axios'
+import { APPLICATION_API_END_POINT } from '@/utils/constant'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { setAllApplicants } from '@/redux/applicationSlice'
 
 const Applicants = () => {
+
+    const params = useParams();
+    const dispatch = useDispatch();
+    const {allApplicants} = useSelector(store=>store.application);
+
+    useEffect(() => {
+        const fecthAllApplicants = async () => {
+            try {
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/getallapplicants`, {
+                    withCredentials: true
+                });
+
+                if (res.data.success) {
+                    dispatch(setAllApplicants(res.data.allApplicants));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fecthAllApplicants();
+    }, [])
+
     return (
         <div>
             <Navbar />
             <div className='max-w-7xl mx-auto'>
-                <h1 className='font-bold text-xl my-5'>Applicants (3)</h1>
+                <h1 className='font-bold text-xl my-5'>Applicants {allApplicants.applications.length}</h1>
                 <ApplicantsTable />
             </div>
         </div>
--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -2,10 +2,35 @@ import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { MoreHorizontal } from 'lucide-react';
+import { useSelector } from 'react-redux';
+import { toast } from 'sonner';
+import axios from 'axios';
+import { APPLICATION_API_END_POINT } from '@/utils/constant';
 
 const shortlistingStatusArr = ["Accepted", "Rejected"];
 
 const ApplicantsTable = () => {
+
+    const { allApplicants } = useSelector(store => store.application);
+
+    const shortlistingStatusHandler = async (id,status) => {
+        try {
+            const res = await axios.post(`${APPLICATION_API_END_POINT}/updatestatus/${id}/update`,{status},{
+                headers:{
+                    "Content-Type":'application/json'
+                },
+                withCredentials:true
+            });
+            
+            if(res.data.success){
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log(error);
+        }
+    }
+
     return (
         <div>
             <Table>
@@ -16,34 +41,46 @@ const ApplicantsTable = () => {
                         <TableHead>Email</TableHead>
                         <TableHead>Contact</TableHead>
                         <TableHead>Resume</TableHead>
+                        <TableHead>Application Date</TableHead>
                         <TableHead className="text-right">Action</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <tr>
-                        <TableCell>Fulll Name</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Contact</TableCell>
-                        <TableCell>Resume</TableCell>
-                        <TableCell className="text-right">
-                            <Popover>
-                                <PopoverTrigger>
-                                    <MoreHorizontal />
-                                </PopoverTrigger>
-                                <PopoverContent className="w-32">
-                                    {
-                                        shortlistingStatusArr.map((status, index) => {
-                                            return (
-                                                <div key={index} className='flex items-center w-fit my-2 cursor-pointer'>
-                                                    <span>{status}</span>
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </PopoverContent>
-                            </Popover>
-                        </TableCell>
-                    </tr>
+                    {
+                        allApplicants && allApplicants?.applications?.map((item) => {
+                            return (
+                                <tr key={item._id}>
+                                    <TableCell>{item?.applicant?.fullname}</TableCell>
+                                    <TableCell>{item?.applicant?.email}</TableCell>
+                                    <TableCell>{item?.applicant?.phoneNumber}</TableCell>
+                                    <TableCell>
+                                        {
+                                            item?.applicant?.profile?.resume ? <a href={item?.applicant?.profile?.resume} target='_blank' className="text-blue-600 cursor-pointer">{item?.applicant?.profile?.resumeOriginalName}</a> : <span>No Resume</span>
+                                        }
+                                    </TableCell>
+                                    <TableCell>{item?.createdAt?.split("T")[0]}</TableCell>
+                                    <TableCell className="text-right">
+                                        <Popover>
+                                            <PopoverTrigger>
+                                                <MoreHorizontal />
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-32">
+                                                {
+                                                    shortlistingStatusArr.map((status, index) => {
+                                                        return (
+                                                            <div key={index} className='flex items-center w-fit my-2 cursor-pointer' onClick={()=>shortlistingStatusHandler(item._id,status)}>
+                                                                <span>{status}</span>
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+                                            </PopoverContent>
+                                        </Popover>
+                                    </TableCell>
+                                </tr>
+                            )
+                        })
+                    }
                 </TableBody>
             </Table>
         </div>
