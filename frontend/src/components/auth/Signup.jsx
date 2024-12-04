7fae9620a6a64aabfc9c3b114e53b40724cc26aa
Choudhury555 <choudhurypanda143@gmail.com>
2024-12-05 01:04:24 +0530

    If user is already logged in then Login and Signup component is not accessible

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
@@ -24,7 +24,7 @@ const Signup = () => {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const { loading } = useSelector(store => store.auth);
+    const { loading,user } = useSelector(store => store.auth);
 
     const inputChangeHandler = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value })
@@ -67,6 +67,12 @@ const Signup = () => {
             dispatch(setLoading(false));
         }
     }
+
+    useEffect(()=>{
+        if(user){
+            navigate("/");
+        }
+    },[]);
 
     return (
         <div>
